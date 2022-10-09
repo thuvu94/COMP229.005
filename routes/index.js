470bc7d9b16058a5ba1c4f5ea76720f7dfd84750
thuvu94 <thanhthuftu94@gmail.com>
2022-10-08 23:09:18 -0400

    Added view templates and partial templates

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About me' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact me' });
+});
+
+module.exports = router;
